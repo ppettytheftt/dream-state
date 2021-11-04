cc6371e1be06cc0363e1779e1254dabf0fd71782
ppettytheftt <jason.c.coffman23@gmail.com>
2021-11-04 16:06:38 -0500

    sets up the desktop launcher to launch the game

--- desktop/src/com/ppettytheftt/dreamstate/desktop/DesktopLauncher.java
+++ desktop/src/com/ppettytheftt/dreamstate/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.ppettytheftt.dreamstate.DreamState;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Dream State";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new DreamState(), config);
 	}
 }
